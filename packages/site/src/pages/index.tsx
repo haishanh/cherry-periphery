23cbacd6c146f2f81857f4488860bc93259eed95
Haishan <haishanhan@gmail.com>
2022-10-09 13:13:10 +0800

    Add og image

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import cx from 'clsx';
 import Link from '@docusaurus/Link';
+import Head from '@docusaurus/Head';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -28,7 +29,8 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout description={`${siteConfig.tagline}`}>
+    <Layout description={siteConfig.tagline}>
+      <OpenGraphImage />
       <HomepageHeader />
       <main>
         <HomepageFeatures />
@@ -83,5 +85,18 @@ function BigShot() {
         />
       </picture>
     </div>
+  );
+}
+
+function OpenGraphImage() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Head>
+      <meta name="twitter:image:src" content="https://i1.haishan.me/file/pelican/00/RAyOr08o-sScNJGqk9lQT-1200.png" />
+      <meta property="og:image" content="https://i1.haishan.me/file/pelican/00/RAyOr08o-sScNJGqk9lQT-1200.png" />
+      <meta property="og:image:alt" content={siteConfig.tagline} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="600" />
+    </Head>
   );
 }
